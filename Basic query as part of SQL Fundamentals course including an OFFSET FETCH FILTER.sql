50019ad9b39ffd0322f75f83465391a4dc7fd0f2
Maureen Onovae <107254397+Onovae@users.noreply.github.com>
2023-09-04 13:53:21 +0100

    Update Basic query as part of SQL Fundamentals course including an OFFSET FETCH FILTER.sql

--- Basic query as part of SQL Fundamentals course including an OFFSET FETCH FILTER.sql
+++ Basic query as part of SQL Fundamentals course including an OFFSET FETCH FILTER.sql
@@ -1,4 +1,4 @@
---Basic query as part of SQL Fundamentals course including an OFFSET FETCH FILTER
+--Basic query SQL Query including an OFFSET FETCH FILTER
 SELECT
 SalesOrderNumber AS InvoiceNumber,
 OrderDate,
