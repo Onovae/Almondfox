c55fb6bf904798b83308f0e08992a188425593a8
Maureen Onovae <107254397+Onovae@users.noreply.github.com>
2023-09-04 13:54:10 +0100

    Update --Basic query as part of SQL Fundamentals course including a DISTINCT filter.sql

--- --Basic query as part of SQL Fundamentals course including a DISTINCT filter.sql
+++ --Basic query as part of SQL Fundamentals course including a DISTINCT filter.sql
@@ -1,4 +1,4 @@
---Basic query as part of SQL Fundamentals course including a DISTINCT filter
+--Basic SQL query including a DISTINCT filter
 SELECT DISTINCT CustomerKey--DISTINCT can be used to return unique values
 
 FROM FactInternetSales
